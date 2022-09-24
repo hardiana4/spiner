740c71d27de75c8a5f00f933f99dbda360b1caad
Hardiana <87222054+hardiana4@users.noreply.github.com>
2022-09-24 08:53:46 +0700

    Update edit_password.blade.php
    Signed-off-by: Hardiana <87222054+hardiana4@users.noreply.github.com>

--- views/data_kinerja/edit_password.blade.php
+++ views/data_kinerja/edit_password.blade.php
@@ -19,20 +19,46 @@
     <div class="col-sm-12">
   <div class="card" style="width: 25rem;">
       <div class="card-header">
-          <form>
+          <form action="/update/password/pegawai/{{ $user->id }}" method="POST">
+            @csrf
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+            @elseif (session('error'))
+                <div class="alert alert-danger" role="alert">
+                    {{ session('error') }}
+                </div>
+            @endif
               <div class="form-group">
                   <div class="col-lg-15">
-                      <label for="exampleFormControlInput1">Password baru</label>
-                      <input type="nama" class="form-control" id="exampleFormControlInput1" placeholder="Password baru ..">
+                      <label for="password">Password Lama</label>
+                      <input type="password" name="password_lama" id="password" class="form-control
+                      @error('email')
+                      is-invalid
+                      @enderror
+                      " placeholder="Masukan Password Lama">
+                      @error('password')
+                          <div class='invalid-feedback'>
+                              {{ $message }}
+                          </div>
+                      @enderror
+                  </div>
+                  </div>
+              <div class="form-group">
+                  <div class="col-lg-15">
+                      <label for="exampleFormControlInput1">Password Baru</label>
+                      <input type="password" class="form-control" name="password_baru" id="password_baru" placeholder="Masukan Password baru ..">
                   </div>
               </div>
               <div class="form-group">
                   <div class="col-lg-15">
-                      <label for="exampleFormControlInput1">Password baru</label>
-                      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Ulangi Password baru ..">
+                      <label for="exampleFormControlInput1">Konfirmasi Password</label>
+                      <input type="password" class="form-control" name="password_konfirmasi" id="password_konfirmasi" placeholder="Ulangi Password baru ..">
                   </div><br>
-                  <button type="button" class="btn btn-primary">Perbarui</button>
-                  <a href="{{url('pengaturan-pegawai')}}" class="btn btn-warning">Batal</a>
+                  <button type="submit" class="btn btn-primary">Perbarui</button>
+                  <button type="cancel" class="btn btn-warning">Batal</button>
+                  {{-- <a href="{{url('pengaturan-pegawai')}}" class="btn btn-warning">Batal</a> --}}
               </div>
           </form>
       </div>
@@ -41,3 +67,4 @@
   </div>
   
   @endsection
+  
